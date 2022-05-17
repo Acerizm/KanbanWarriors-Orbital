92f74a708a1eae1e6a43be2b28509af7c98cf023
Haiqel <acehql3@gmail.com>
2022-05-18 03:20:13 +0800

    added skeleton of APIs & server configs

---
+++ back-end/back-end/Controllers/KanbanWarriorsAPI.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace back_end.Controllers
+{
+    public class KanbanWarriorsAPI
+    {
+    }
+}
---
+++ back-end/back-end/Services/BackEndServices.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+//using MongoDB.Driver;
+
+
+
+namespace back_end.Services
+{
+    public class BackEndServices
+    {
+    }
+}
