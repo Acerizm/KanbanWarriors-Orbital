dafe9cdadcd32410818d3d98a854692b734c393f
Haiqel <acehql3@gmail.com>
2022-05-19 01:25:16 +0800

    small update

--- back-end/back-end/Controllers/BackgroundImagesController.cs
+++ back-end/back-end/Controllers/BackgroundImagesController.cs
@@ -50,5 +50,84 @@ namespace back_end.Controllers
                 return imagesList;
         }
 
+        /// <summary>
+        /// Gets all of the backgroundImages based on category Id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///      /GetImagesFromCategory/{Id}
+        ///     {
+        ///        "id": "5ce37619a9e07c300c1e6a7d",
+        ///        "category": "Nature",
+        ///        "backgroundImages": [1,2],
+        ///        "cloudinaryRP": "/relative_path"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>       
+        // GET: api/BackgroundImages/GetAllImages
+        [Route("GetImagesFromCategory/{Id}")]
+        [HttpGet]
+        public async Task<ActionResult<Images>> GetImagesFromCategory(string Id)
+        {
+            Images images = await Task.Run(() => backgroundImagesServices.Get(Id));
+            if (images == null)
+                return null;
+            else
+                return images;
+        }
+
+        /// <summary>
+        /// Create a new category of images
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///      /CreateNewCategory
+        ///     {
+        ///        "id": "5cea58888578a12e180e9703",
+        ///        "category": "Wildlife",
+        ///        "backgroundImages": [1,2],
+        ///        "cloudinaryRP": "./."
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>       
+        //POST: create new category of images
+        [Route("CreateNewCategory")]
+        [HttpPost]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult<Images>> CreateNewCategory(string category, List<int> backgroundImages,string cloudinaryRP)
+        {
+            // the loop will continue to run if we are able to find any phone model 
+            // with an exisiting ID
+            Images newCategoryOfImages = new Images();
+            // this is bad algo
+            // o(n) when data is damn huge
+            // improve this algo
+            while (true)
+            {
+                //create a new objectId
+                var objectId = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+                //check for the objectId if it exists
+                var checkId = backgroundImagesServices.Get(objectId);
+                if (checkId == null)
+                {
+                    newCategoryOfImages.Id = objectId;
+                    newCategoryOfImages.category = category;
+                    newCategoryOfImages.backgroundImages = backgroundImages;
+                    newCategoryOfImages.cloudinaryRP = cloudinaryRP;
+                    await Task.Run(() => backgroundImagesServices.Create(newCategoryOfImages));
+                    return newCategoryOfImages;
+                }
+                else
+                    continue;
+            }
+        }
+
+
     }
 }
