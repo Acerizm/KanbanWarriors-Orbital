6d742ad54909c3d124925eb99314caafdbc395d3
AshiqurRah <89114596+AshiqurRah@users.noreply.github.com>
2022-06-30 10:20:28 +0800

    Minor Edits for Pomodoro

--- front-end/src/Components/SocketClient/index.js
+++ front-end/src/Components/SocketClient/index.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 //for production
-export const socket = io.connect("http://159.223.91.154:4000");
+// export const socket = io.connect("http://159.223.91.154:4000");
 
 //for development
-//export const socket = io.connect("http://localhost:4000");
+export const socket = io.connect("http://localhost:4000");
